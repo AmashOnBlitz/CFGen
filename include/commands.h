f7a31ee93deb89b54b3bdb6b12f8e6c1eb3e487c
AmashOnBlitz <myselfamash@gmail.com>
2026-02-17 21:49:01 +0530

    Chore : Rebased version 2 branch into main

--- include/commands.h
+++ include/commands.h
@@ -1,16 +1,18 @@
 #pragma once
 
-#define COMMAND_HELP        "-h     "
-#define COMMAND_GEN         "-gen   "
-#define COMMAND_REGISTER    "-reg   "
-#define COMMAND_DELETE      "-del   "
-#define COMMAND_SHOW        "-show  "
+#define COMMAND_HELP        "-h        "
+#define COMMAND_GEN         "-gen      "
+#define COMMAND_REGISTER    "-reg      "
+#define COMMAND_DELETE      "-del      "
+#define COMMAND_SHOW        "-show     "
+#define COMMAND_VERSION     "-version  "
 
 #define COMMAND_HELP_TRIMMED        "-h"
 #define COMMAND_GEN_TRIMMED         "-gen"
 #define COMMAND_REGISTER_TRIMMED    "-reg"
 #define COMMAND_DELETE_TRIMMED      "-del"
 #define COMMAND_SHOW_TRIMMED        "-show"
+#define COMMAND_VERSION_TRIMMED     "-version"
 
 #define COMMAND_TEMPLATE_SUB    "-t"    
 #define COMMAND_MACRO_SUB       "-m"
